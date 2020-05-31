488c142a3ee519b4731dd4c2197c7d93daf1de9c
unknown <1147859273@qq.com>
2020-05-31 10:45:00 +0800

    添加按钮和输入框文档，完善了按钮文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,9 +23,9 @@ module.exports = {
 			// {title:'demo',path:'/demo'},
 			{title:'前言',path:'/about'},
 			{title:'开始',path:'/start'},
-			{title:'按钮',path:'/button'},
-			{title:'图标',path:'/icon'},
-			{title:'输入框',path:'/input'},
+			{title:'Button按钮',path:'/button'},
+			{title:'Icon图标',path:'/icon'},
+			{title:'Input输入框',path:'/input'},
 		]
 	},
 	sidebar: [
